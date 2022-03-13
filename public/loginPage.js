34bee58790c8448643abd99a8ddd5024f04235c3
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2022-03-13 14:37:45 +0200

    Add files via upload

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,14 +1,15 @@
 "use strict"
 const userForm1 = new UserForm();
-userForm1.loginFormCallback = function () {
-    
-    setTimeout(()=> {
+userForm1.loginFormCallback = function ({login, password}) {
+    const data ={login, password};
+    function func (data) {
         if (ApiConnector.login) {
             location.reload();
         } else {
-        setLoginErrorMessage(`Пользователь с логином ${data.login}  и указанным паролем не найден`); 
-        };
-    
-    }, 2000);
-}
+            setLoginErrorMessage(`Пользователь с логином ${data.login}  и указанным паролем не найден`); 
+        }
+    }
 
+    ApiConnector.login({login, password}, func);
+
+    }
