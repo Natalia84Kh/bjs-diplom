d2d5512726b9f20cfb0bec150c6fe1218370c6dc
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2022-03-20 22:37:53 +0200

    Add files via upload

--- public/homePage.js
+++ public/homePage.js
@@ -1,3 +1,4 @@
+
 const logoutButton = new LogoutButton ();
 
 logoutButton.action = () => {
@@ -7,3 +8,23 @@ logoutButton.action = () => {
     }
   })
 }
+
+ApiConnector.current((response)=> {
+    if(response.success){
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
+const ratesBoard = new RatesBoard ();
+
+ratesBoard.ratesBoardCallback = (data) => {
+    ApiConnector.getStocks((response) => {
+        if(response.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+        }
+    })
+};
+
+ratesBoard.ratesBoardCallback();
+setInterval(ratesBoard.ratesBoardCallback, 20000);
