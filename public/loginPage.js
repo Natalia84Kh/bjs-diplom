5079f8e646cf6dd990bff88d90e6e7d9e5f21930
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2022-03-20 20:24:35 +0200

    Add files via upload

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,23 +1,23 @@
 "use strict"
-const userForm1 = new UserForm();
-userForm1.loginFormCallback =  (data) => {
+const userForm = new UserForm();
+userForm.loginFormCallback =  (data) => {
 
-    ApiConnector.login(data, func.call = (this, success) =>{
-        if(success) {
+    ApiConnector.login(data, (response) =>{
+        if(response.success) {
             location.reload();
         } else {
-            userForm1.setLoginErrorMessage(error);
+            userForm.setLoginErrorMessage(response.error);
           }
     } );
     
 }
 
-userForm1.registerFormCallback = (data) => {
-    ApiConnector.register(data, func.call = (this, success) => {
-        if(success){
+userForm.registerFormCallback = (data) => {
+    ApiConnector.register(data, (response) => {
+        if(response.success){
             location.reload();
         } else {
-            userForm1.setRegisterErrorMessage(error);
+            userForm.setRegisterErrorMessage(response.error);
         }
     } );
 }
