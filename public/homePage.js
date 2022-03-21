490500cc50f33b6fe20c4648c1c6596eb8f5fb00
Natalia84Kh <73022006+Natalia84Kh@users.noreply.github.com>
2022-03-21 12:42:48 +0200

    Add files via upload

--- public/homePage.js
+++ public/homePage.js
@@ -1,4 +1,5 @@
 
+
 const logoutButton = new LogoutButton ();
 
 logoutButton.action = () => {
@@ -28,3 +29,76 @@ ratesBoard.ratesBoardCallback = (data) => {
 
 ratesBoard.ratesBoardCallback();
 setInterval(ratesBoard.ratesBoardCallback, 20000);
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if(response.success){
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(response.success, "Пополнение баланса прошело успешно");
+    } else {
+      moneyManager.setMessage(response.success, "Пополнение баланса завершилось ошибкой");
+    }
+  });
+} 
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if(response.success){
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(response.success, "Конвертация прошела успешно");
+    } else {
+      moneyManager.setMessage(response.success, "Конвертация завершилась ошибкой");
+    }
+  });
+}
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if(response.success){
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(response.success, "Перевод средств прошел успешно");
+    } else {
+      moneyManager.setMessage(response.success, "Перевод средств завершился ошибкой");
+    } 
+  });
+}
+
+const favoritesWidget = new FavoritesWidget ();
+
+ ApiConnector.getFavorites ((response) => {
+  if(response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  }
+ });
+
+ favoritesWidget.addUserCallback = (data) => {
+   ApiConnector.addUserToFavorites(data, (response) => {
+    if(response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      favoritesWidget.setMessage(response.success, "Добавление пользователя прошло успешно")
+    } else {
+        favoritesWidget.setMessage(response.success, response.error);
+    }
+   
+   });
+ }
+
+ favoritesWidget.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+   if(response.success) {
+     favoritesWidget.clearTable();
+     favoritesWidget.fillTable(response.data);
+     moneyManager.updateUsersList(response.data);
+     favoritesWidget.setMessage(response.success, "Удаление пользователя прошло успешно")
+   } else {
+       favoritesWidget.setMessage(response.success, response.error);
+   }
+  
+  });
+}
